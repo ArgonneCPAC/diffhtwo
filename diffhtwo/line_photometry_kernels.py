46e3a92b54beeace85912ed55daecef12b635680
Andrew Hearin <ahearin@anl.gov>
2024-03-27 10:01:54 -0500

    Finished bare kernel _ab_flux_line_kern

--- diffhtwo/line_photometry_kernels.py
+++ diffhtwo/line_photometry_kernels.py
@@ -6,17 +6,12 @@ from jax import config
 from jax import jit as jjit
 from jax import numpy as jnp
 
+from .line_luminosity_kernels import LINE_LUM_NORM
+
 config.update("jax_enable_x64", True)
 L_AB = 4.4659e20  # erg/s/Hz
-C_ANGSTROMS = 1e10 * 2.997e8
-
-
-@jjit
-def _filter_flux_ab0_at_10pc(wave_filter, trans_filter):
-    filter_flux_ab0_by_lab = _filter_flux_ab0_at_10pc_order_unity(
-        wave_filter, trans_filter
-    )
-    return filter_flux_ab0_by_lab * L_AB
+C_ANGSTROMS = 1e10 * 2.997e8  # angstrom/s
+LINE_FLUX_NORM = LINE_LUM_NORM / C_ANGSTROMS / L_AB
 
 
 @jjit
@@ -30,32 +25,15 @@ def _filter_flux_ab0_at_10pc_order_unity(wave_filter, trans_filter):
 def _ab_flux_line(line_angstrom, line_lum_cgs, tcurve_wave_aa, tcurve_trans):
     trans_at_line = jnp.interp(line_angstrom, tcurve_wave_aa, tcurve_trans)
     numerator = (line_angstrom / C_ANGSTROMS) * trans_at_line * line_lum_cgs
-    filter_flux_ab0 = _filter_flux_ab0_at_10pc(tcurve_wave_aa, tcurve_trans)
+    filter_flux_ab0 = L_AB * _filter_flux_ab0_at_10pc_order_unity(
+        tcurve_wave_aa, tcurve_trans
+    )
     ab_flux = numerator / filter_flux_ab0
     return ab_flux
 
 
 @jjit
-def _ab_flux_line_order_unity(line_angstrom, tcurve_wave_aa, tcurve_trans):
-    filter_flux_ab0_by_lab = _filter_flux_ab0_at_10pc_order_unity(
-        tcurve_wave_aa, tcurve_trans
-    )
-    trans_at_line = jnp.interp(line_angstrom, tcurve_wave_aa, tcurve_trans)
-    return trans_at_line / filter_flux_ab0_by_lab
-
-
-@jjit
-def _ab_filter_flux_factor_from_precomputed(
-    line_wave_aa_obs, filter_flux_ab0, trans_at_line_wave_obs
-):
-    numerator = (line_wave_aa_obs / C_ANGSTROMS) * trans_at_line_wave_obs
-    ab_flux_factor = numerator / filter_flux_ab0
-    return ab_flux_factor
-
-
-@jjit
-def _get_precomputed_quantities(line_wave_aa, redshift, wave_filter, trans_filter):
-    line_wave_aa_obs = line_wave_aa * (1.0 + redshift)
-    filter_flux_ab0_order_unity = _filter_flux_ab0_at_10pc_order_unity(
-        wave_filter, trans_filter
-    )
+def _ab_flux_line_kern(line_wave_aa, line_trans, line_ltot_scaled, mstar, f_ab):
+    x = line_wave_aa * line_trans * line_ltot_scaled * mstar / f_ab
+    filter_flux = x * LINE_FLUX_NORM
+    return filter_flux
--- diffhtwo/tests/test_line_photometry_kernels.py
+++ diffhtwo/tests/test_line_photometry_kernels.py
@@ -7,24 +7,6 @@ from dsps.utils import triweight_gaussian
 from .. import line_photometry_kernels as lpk
 
 
-def test_ab_filter_flux_factor_from_precomputed():
-    halpha = 6565.0
-    z_obs = 0.5
-    line_wave_aa_obs = halpha * (1.0 + z_obs)
-
-    line_lum_cgs = 3.5e39
-    filter_flux_ab0 = 4.45e19
-    trans_at_line_wave_obs = 0.5
-    args = line_wave_aa_obs, filter_flux_ab0, trans_at_line_wave_obs
-    for arg in args:
-        assert np.all(np.isfinite(arg))
-    ab_flux_factor = lpk._ab_filter_flux_factor_from_precomputed(*args)
-    assert np.all(np.isfinite(ab_flux_factor))
-    assert np.all(np.isfinite(filter_flux_ab0))
-    ab_flux = ab_flux_factor * line_lum_cgs
-    assert np.all(np.isfinite(ab_flux))
-
-
 def test_ab_flux_line():
     halpha_wave_aa_obs = 6565.0
     line_lum_cgs = 3.5e39
