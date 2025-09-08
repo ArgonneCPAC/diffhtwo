3e5a2e7c2dce2915d211f5dfb543b3708dd723e4
Kumail Zaidi <kumail.tufts@gmail.com>
2025-09-08 14:28:42 -0400

    Update diffstarpop_halpha_opt.py

--- diffhtwo/experimental/diffstarpop_halpha_opt.py
+++ diffhtwo/experimental/diffstarpop_halpha_opt.py
@@ -49,8 +49,8 @@ def _loss_kern(
     lf_smooth_ms_pred = halpha_lf_pred.halpha_L_cgs_smooth_ms
     lf_q_pred = halpha_lf_pred.halpha_L_cgs_q
 
-    print(np.allclose(lf_smooth_ms_true, lf_smooth_ms_pred))
-    print(np.allclose(lf_q_true, lf_q_pred))
+    print(jnp.allclose(lf_smooth_ms_true, lf_smooth_ms_pred))
+    print(jnp.allclose(lf_q_true, lf_q_pred))
 
     return _mse(lf_smooth_ms_true, lf_smooth_ms_pred, lf_q_true, lf_q_pred)
 
