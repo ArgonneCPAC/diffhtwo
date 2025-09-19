286e9ae9f32e66c75aba9b42f979617f58d14ff3
Kumail Zaidi <kumail.tufts@gmail.com>
2025-09-19 18:02:34 -0400

    get halpha_L_cgs and its weights for bursty population as well

--- diffhtwo/experimental/diffstarpop_halpha.py
+++ diffhtwo/experimental/diffstarpop_halpha.py
@@ -8,6 +8,7 @@ from collections import namedtuple
 
 import jax
 import jax.numpy as jnp
+from diffsky.burstpop import diffqburstpop_mono, freqburst_mono
 from diffsky.experimental.lc_phot_kern import diffstarpop_lc_cen_wrapper
 from dsps.metallicity import umzr
 from dsps.sed import metallicity_weights as zmetw
@@ -38,6 +39,12 @@ _calc_lgmet_weights_galpop = jjit(
     vmap(zmetw.calc_lgmet_weights_from_lognormal_mdf, in_axes=_M)
 )
 
+_B = (None, 0, 0, None, 0)
+_calc_bursty_age_weights_vmap = jjit(
+    vmap(
+        diffqburstpop_mono.calc_bursty_age_weights_from_diffburstpop_params, in_axes=_B
+    )
+)
 
 _AGEPOP = (None, 0, None, 0)
 calc_age_weights_from_sfh_table_vmap = jjit(
@@ -75,6 +82,23 @@ def diffstarpop_halpha_kern(
         t_table, diffstar_galpop.sfh_q, ssp_data.ssp_lg_age_gyr, t_obs
     )
 
+    # get bursty age weights
+    _args = (
+        spspop_params.burstpop_params,
+        diffstar_galpop.logsm_obs_ms,
+        diffstar_galpop.logssfr_obs_ms,
+        ssp_data.ssp_lg_age_gyr,
+        smooth_age_weights_ms,
+    )
+    bursty_age_weights_ms, burst_params = _calc_bursty_age_weights_vmap(*_args)
+
+    # get p_burst_ms
+    p_burst_ms = freqburst_mono.get_freqburst_from_freqburst_params(
+        spspop_params.burstpop_params.freqburst_params,
+        diffstar_galpop.logsm_obs_ms,
+        diffstar_galpop.logssfr_obs_ms,
+    )
+
     # get metallicity weights
     lgmet_med_ms = umzr.mzr_model(diffstar_galpop.logsm_obs_ms, t_obs, *mzr_params)
     lgmet_med_q = umzr.mzr_model(diffstar_galpop.logsm_obs_q, t_obs, *mzr_params)
@@ -95,25 +119,36 @@ def diffstarpop_halpha_kern(
     _w_lgmet_ms = lgmet_weights_ms.reshape((n_gals, n_met, 1))
     ssp_weights_smooth_ms = _w_lgmet_ms * _w_age_ms
 
-    _mstar_ms = 10**diffstar_galpop.logsm_obs_ms
+    _w_age_bursty_ms = bursty_age_weights_ms.reshape((n_gals, 1, n_age))
+    ssp_weights_bursty_ms = _w_lgmet_ms * _w_age_bursty_ms
+
     _mstar_q = 10**diffstar_galpop.logsm_obs_q
+    _mstar_ms = 10**diffstar_galpop.logsm_obs_ms
+
+    integrand_q = ssp_halpha_luminosity * ssp_weights_q
+    halpha_L_cgs_q = jnp.sum(integrand_q, axis=(1, 2)) * (L_SUN_CGS * _mstar_q)
 
     integrand_smooth_ms = ssp_halpha_luminosity * ssp_weights_smooth_ms
     halpha_L_cgs_smooth_ms = jnp.sum(integrand_smooth_ms, axis=(1, 2)) * (
         L_SUN_CGS * _mstar_ms
     )
 
-    integrand_q = ssp_halpha_luminosity * ssp_weights_q
-    halpha_L_cgs_q = jnp.sum(integrand_q, axis=(1, 2)) * (L_SUN_CGS * _mstar_q)
+    integrand_bursty_ms = ssp_halpha_luminosity * ssp_weights_bursty_ms
+    halpha_L_cgs_bursty_ms = jnp.sum(integrand_bursty_ms, axis=(1, 2)) * (
+        L_SUN_CGS * _mstar_ms
+    )
 
     weights_q = diffstar_galpop.frac_q
-    weights_smooth_ms = 1 - diffstar_galpop.frac_q
+    weights_smooth_ms = (1 - diffstar_galpop.frac_q) * (1 - p_burst_ms)
+    weights_bursty_ms = (1 - diffstar_galpop.frac_q) * p_burst_ms
 
     halpha_L = LCLINE_EMPTY._replace(
-        halpha_L_cgs_smooth_ms=halpha_L_cgs_smooth_ms,
         halpha_L_cgs_q=halpha_L_cgs_q,
-        weights_smooth_ms=weights_smooth_ms,
+        halpha_L_cgs_smooth_ms=halpha_L_cgs_smooth_ms,
+        halpha_L_cgs_bursty_ms=halpha_L_cgs_bursty_ms,
         weights_q=weights_q,
+        weights_smooth_ms=weights_smooth_ms,
+        weights_bursty_ms=weights_bursty_ms,
     )
 
     return halpha_L
