22884a0343317ddceada578ca899de9d65ff10ee
Kumail Zaidi <kumail.tufts@gmail.com>
2025-10-30 10:32:18 -0500

    calculate line's band magnitude for a population

--- diffhtwo/experimental/line_phot_kern.py
+++ diffhtwo/experimental/line_phot_kern.py
@@ -1,5 +1,6 @@
 import jax.numpy as jnp
 from jax import jit as jjit
+from jax import vmap
 
 from .defaults import C_ANGSTROMS
 
@@ -36,3 +37,38 @@ def flux_density_filter_aa(
     line_app_flux_band_cgs = trans_at_line * line_app_flux_cgs
 
     return line_app_flux_band_cgs / tcurve_equivalent_width_aa
+
+
+@jjit
+def line_mag(line_obs_aa, line_L_cgs, redshift, d_L, tcurve_wave_aa, tcurve_trans):
+    # forward model line flux
+    line_flux_app_cgs = _flux_app_from_luminosity(line_L_cgs, redshift, d_L)
+
+    # equivalent_width
+    equivalent_width_aa = _tcurve_equivalent_width(tcurve_wave_aa, tcurve_trans)
+
+    # flux_density_aa
+    flux_density_filter_AA = flux_density_filter_aa(
+        line_obs_aa,
+        line_flux_app_cgs,
+        tcurve_wave_aa,
+        tcurve_trans,
+        equivalent_width_aa,
+    )
+
+    # flux_density_hz
+    flux_density_filter_hz = _flux_density_aa_to_hz(flux_density_filter_AA, line_obs_aa)
+
+    # mag_ab
+    mag_ab = _flux_density_hz_to_mag_ab(flux_density_filter_hz)
+
+    return mag_ab
+
+
+_M = (0, 0, 0, 0, None, None)
+line_mag_vmap = jjit(
+    vmap(
+        line_mag,
+        in_axes=_M,
+    )
+)
