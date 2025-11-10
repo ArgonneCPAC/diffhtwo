75b3b30aa389762792081bd8050027a5a166723b
Kumail Zaidi <kumail.tufts@gmail.com>
2025-11-10 10:08:22 -0500

    /lc_vol

--- diffhtwo/experimental/nd_mag.py
+++ diffhtwo/experimental/nd_mag.py
@@ -115,6 +115,6 @@ def nd_mag_kern(
         lh_centroids_hi,
     )
 
-    nd_model = nd_q + nd_smooth_ms + nd_bursty_ms
+    nd_model = (nd_q + nd_smooth_ms + nd_bursty_ms) / lc_vol
 
     return nd_model
