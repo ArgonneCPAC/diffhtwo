14b073ccb8871d445a54d8a94a40ddaddc5a04fc
Kumail Zaidi <kumail.tufts@gmail.com>
2025-11-11 16:41:57 -0500

    Update nd_mag.py

--- diffhtwo/experimental/nd_mag.py
+++ diffhtwo/experimental/nd_mag.py
@@ -78,14 +78,14 @@ def nd_mag_kern(
     # mag_colors_q = mag_colors_q.T
 
     mag_colors_smooth_ms = lc_phot.obs_mags_smooth_ms[:, 0]
-    mag_colors_smooth_ms = mag_colors_smooth_ms.reshape(mag_colors_smooth_ms.size, 1)
+    # mag_colors_smooth_ms = mag_colors_smooth_ms.reshape(mag_colors_smooth_ms.size, 1)
     for band in range(1, n_bands):
         color = mag_colors_smooth_ms - lc_phot.obs_mags_smooth_ms[:, band]
         mag_colors_smooth_ms = jnp.vstack((mag_colors_smooth_ms, color))
     # mag_colors_smooth_ms = mag_colors_smooth_ms.T
 
     mag_colors_bursty_ms = lc_phot.obs_mags_bursty_ms[:, 0]
-    mag_colors_bursty_ms = mag_colors_bursty_ms.reshape(mag_colors_bursty_ms.size, 1)
+    # mag_colors_bursty_ms = mag_colors_bursty_ms.reshape(mag_colors_bursty_ms.size, 1)
     for band in range(1, n_bands):
         color = mag_colors_bursty_ms - lc_phot.obs_mags_bursty_ms[:, band]
         mag_colors_bursty_ms = jnp.vstack((mag_colors_bursty_ms, color))
