647d745b3481114f278323ea8e825d3f80edf788
Kumail Zaidi <kumail.tufts@gmail.com>
2025-12-16 21:41:50 -0600

    compare two spspop_params

--- diffhtwo/experimental/diagnostics/plot_mag_color_1d_hist.py
+++ diffhtwo/experimental/diagnostics/plot_mag_color_1d_hist.py
@@ -20,7 +20,8 @@ except ImportError:
 
 def plot_n_ugriz(
     diffstarpop_params,
-    spspop_params,
+    spspop_params1,
+    spspop_params2,
     tcurves,
     mag_column,
     dmag,
@@ -55,9 +56,9 @@ def plot_n_ugriz(
 
     wave_eff_table = lc_phot_kern.get_wave_eff_table(z_phot_table, tcurves)
 
-    ran_key, phot_key = jran.split(ran_key, 2)
-    phot_args = (
-        phot_key,
+    ran_key, phot_key1 = jran.split(ran_key, 2)
+    phot_args1 = (
+        phot_key1,
         lc_halopop["z_obs"],
         lc_halopop["t_obs"],
         lc_halopop["mah_params"],
@@ -69,15 +70,128 @@ def plot_n_ugriz(
         wave_eff_table,
         diffstarpop_params,
         mzr_params,
-        spspop_params,
+        spspop_params1,
         scatter_params,
         ssp_err_pop_params,
         cosmo_params,
         fb,
     )
 
-    lc_phot = lc_phot_kern.multiband_lc_phot_kern(*phot_args)
+    lc_phot1 = lc_phot_kern.multiband_lc_phot_kern(*phot_args1)
+    num_halos, n_bands = lc_phot1.obs_mags_q.shape
 
+    (
+        obs_colors_mag_q1,
+        obs_colors_mag_smooth_ms1,
+        obs_colors_mag_bursty_ms1,
+    ) = get_obs_colors_mag(lc_phot1, mag_column)
+
+    ran_key, phot_key2 = jran.split(ran_key, 2)
+    phot_args2 = (
+        phot_key2,
+        lc_halopop["z_obs"],
+        lc_halopop["t_obs"],
+        lc_halopop["mah_params"],
+        lc_halopop["logmp0"],
+        t_table,
+        ssp_data,
+        precomputed_ssp_mag_table,
+        z_phot_table,
+        wave_eff_table,
+        diffstarpop_params,
+        mzr_params,
+        spspop_params2,
+        scatter_params,
+        ssp_err_pop_params,
+        cosmo_params,
+        fb,
+    )
+
+    lc_phot2 = lc_phot_kern.multiband_lc_phot_kern(*phot_args2)
+    num_halos, n_bands = lc_phot2.obs_mags_q.shape
+
+    (
+        obs_colors_mag_q2,
+        obs_colors_mag_smooth_ms2,
+        obs_colors_mag_bursty_ms2,
+    ) = get_obs_colors_mag(lc_phot2, mag_column)
+
+    fig, ax = plt.subplots(1, 5, figsize=(14, 3))
+    fig.subplots_adjust(left=0.275, hspace=0, top=0.95, right=0.87, wspace=0.3)
+
+    color_bin_edges = np.arange(-0.5 - dmag / 2, 2.0, dmag)
+    mag_bin_edges = np.arange(18.0 - dmag / 2, 26.0, dmag)
+
+    weights1 = np.concatenate(
+        [
+            lc_phot1.weights_q * (1 / lc_vol_mpc3),
+            lc_phot1.weights_smooth_ms * (1 / lc_vol_mpc3),
+            lc_phot1.weights_bursty_ms * (1 / lc_vol_mpc3),
+        ]
+    )
+
+    weights2 = np.concatenate(
+        [
+            lc_phot2.weights_q * (1 / lc_vol_mpc3),
+            lc_phot2.weights_smooth_ms * (1 / lc_vol_mpc3),
+            lc_phot2.weights_bursty_ms * (1 / lc_vol_mpc3),
+        ]
+    )
+    for i in range(0, n_bands):
+        if i == n_bands - 1:
+            bins = mag_bin_edges
+        else:
+            bins = color_bin_edges
+
+        obs_colors_mag1 = np.concatenate(
+            [
+                obs_colors_mag_q1[:, i],
+                obs_colors_mag_smooth_ms1[:, i],
+                obs_colors_mag_bursty_ms1[:, i],
+            ]
+        )
+
+        ax[i].hist(
+            obs_colors_mag1,
+            weights=weights1,
+            width=dmag,
+            bins=bins,
+            facecolor="orange",
+            alpha=0.7,
+            label="1",
+        )
+
+        obs_colors_mag2 = np.concatenate(
+            [
+                obs_colors_mag_q2[:, i],
+                obs_colors_mag_smooth_ms2[:, i],
+                obs_colors_mag_bursty_ms2[:, i],
+            ]
+        )
+
+        ax[i].hist(
+            obs_colors_mag2,
+            weights=weights2,
+            width=dmag,
+            bins=bins,
+            histtype="step",
+            color="green",
+            alpha=0.7,
+            label="2",
+        )
+
+    ax[0].set_ylabel("number density [Mpc$^{-3}$]")
+    ax[0].set_xlabel("MegaCam_uS - HSC_g [AB]")
+    ax[1].set_xlabel("HSC_g- HSC_r [AB]")
+    ax[2].set_xlabel("HSC_r - HSC_i [AB]")
+    ax[3].set_xlabel("HSC_i - HSC_z [AB]")
+    ax[4].set_xlabel("HSC_i [AB]")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def get_obs_colors_mag(lc_phot, mag_column):
     num_halos, n_bands = lc_phot.obs_mags_q.shape
 
     obs_colors_mag_q = []
@@ -111,47 +225,4 @@ def plot_n_ugriz(
     obs_colors_mag_bursty_ms.append(obs_mag_bursty_ms)
     obs_colors_mag_bursty_ms = jnp.asarray(obs_colors_mag_bursty_ms).T
 
-    fig, ax = plt.subplots(1, 5, figsize=(14, 3))
-    fig.subplots_adjust(left=0.275, hspace=0, top=0.95, right=0.87, wspace=0.3)
-
-    color_bin_edges = np.arange(-0.5 - dmag / 2, 2.0, dmag)
-    mag_bin_edges = np.arange(18.0 - dmag / 2, 26.0, dmag)
-    weights = np.concatenate(
-        [
-            lc_phot.weights_q * (1 / lc_vol_mpc3),
-            lc_phot.weights_smooth_ms * (1 / lc_vol_mpc3),
-            lc_phot.weights_bursty_ms * (1 / lc_vol_mpc3),
-        ]
-    )
-    for i in range(0, n_bands):
-        if i == n_bands - 1:
-            bins = mag_bin_edges
-        else:
-            bins = color_bin_edges
-
-        obs_colors_mag = np.concatenate(
-            [
-                obs_colors_mag_q[:, i],
-                obs_colors_mag_smooth_ms[:, i],
-                obs_colors_mag_bursty_ms[:, i],
-            ]
-        )
-
-        ax[i].hist(
-            obs_colors_mag,
-            weights=weights,
-            width=dmag,
-            bins=bins,
-            facecolor="orange",
-            alpha=0.7,
-        )
-
-    ax[0].set_ylabel("number density [Mpc$^{-3}$]")
-    ax[0].set_xlabel("MegaCam_uS - HSC_g [AB]")
-    ax[1].set_xlabel("HSC_g- HSC_r [AB]")
-    ax[2].set_xlabel("HSC_r - HSC_i [AB]")
-    ax[3].set_xlabel("HSC_i - HSC_z [AB]")
-    ax[4].set_xlabel("HSC_i [AB]")
-
-    plt.tight_layout()
-    plt.show()
+    return obs_colors_mag_q, obs_colors_mag_smooth_ms, obs_colors_mag_bursty_ms
