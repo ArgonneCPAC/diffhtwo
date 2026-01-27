2b11e3cc82b7b1dff7e29e7ee673bd90215ffe22
Kumail Zaidi <kumail.tufts@gmail.com>
2026-01-27 10:47:47 -0500

    Update plot_mag_color_1d_hist.py

--- diffhtwo/experimental/diagnostics/plot_mag_color_1d_hist.py
+++ diffhtwo/experimental/diagnostics/plot_mag_color_1d_hist.py
@@ -424,7 +424,7 @@ def plot_n_ugriz(
     )
 
     # Plot corner
-    ranges = [(-1, 2.5), (-1, 2.0), (-0.5, 1.0), (-0.5, 1.0), (18, mag_thresh)]
+    ranges = [(-1, 2.5), (-1, 2.0), (-0.5, 1.5), (-0.5, 1.5), (18, mag_thresh)]
     labels = [
         r"$uS_{MegaCam} - g_{HSC} [AB]$",
         r"$g_{HSC} - r_{HSC} [AB]$",
