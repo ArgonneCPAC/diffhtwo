62fbe4e59c0425817a0e92061597a5238e9d8d29
Kumail Zaidi <kumail.tufts@gmail.com>
2026-01-29 09:42:57 -0600

    Update plot_mag_color_1d_hist.py

--- diffhtwo/experimental/diagnostics/plot_mag_color_1d_hist.py
+++ diffhtwo/experimental/diagnostics/plot_mag_color_1d_hist.py
@@ -185,9 +185,7 @@ def plot_n_mag_ugriz(
     )
 
     fig, ax = plt.subplots(1, 5, figsize=(11.5, 3))
-    fig.subplots_adjust(
-        left=0.1, hspace=0, top=0.85, right=0.99, bottom=0.2, wspace=0.0
-    )
+    fig.subplots_adjust(left=0.1, hspace=0, top=0.8, right=0.99, bottom=0.2, wspace=0.0)
     fig.suptitle(title)
 
     mag_bin_edges = np.arange(18.0 - dmag / 2, mag_thresh, dmag)
@@ -518,9 +516,7 @@ def plot_n_ugriz(
 
     # Plot 1D histograms
     fig, ax = plt.subplots(1, 5, figsize=(11.5, 3))
-    fig.subplots_adjust(
-        left=0.1, hspace=0, top=0.85, right=0.99, bottom=0.2, wspace=0.0
-    )
+    fig.subplots_adjust(left=0.1, hspace=0, top=0.8, right=0.99, bottom=0.2, wspace=0.0)
     fig.suptitle(title)
 
     color_bin_edges = np.arange(-0.5 - dmag / 2, 2.0, dmag)
