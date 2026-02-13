41f563a2e8fbcdd37249234c973d1a04fd1acfa0
Kumail Zaidi <kumail.tufts@gmail.com>
2026-02-13 14:06:53 -0600

    unjit zbin_volume

--- diffhtwo/experimental/utils.py
+++ diffhtwo/experimental/utils.py
@@ -36,7 +36,6 @@ def safe_log10(x, EPS=1e-12):
     return jnp.log(jnp.clip(x, EPS, jnp.inf)) / jnp.log(10.0)
 
 
-@jjit
 def zbin_volume(sky_area_degsq, zlow=0.2, zhigh=0.5, slices=1000):
     """
     Calculate Comoving Volume in Mpc3/h units for a given z-bin and area of survey.
@@ -45,12 +44,12 @@ def zbin_volume(sky_area_degsq, zlow=0.2, zhigh=0.5, slices=1000):
     slices: number of slices used for integration of dV/dz over z
     A: Survey area in deg2
     """
-    z = jnp.linspace(zlow, zhigh, slices)
-    dV_dz = jnp.zeros(len(z))
+    z = np.linspace(zlow, zhigh, slices)
+    dV_dz = np.zeros(len(z))
     A = sky_area_degsq * u.deg**2
     for i in range(0, len(z)):
         dV_dz[i] = COSMO.differential_comoving_volume(z[i]).value
-    volume = (jnp.trapezoid(dV_dz, z) * u.Mpc**3 / u.sr) * A.to(u.sr)
+    volume = (np.trapezoid(dV_dz, z) * u.Mpc**3 / u.sr) * A.to(u.sr)
 
     # Mpc3 units (no h dependence)
     return volume
