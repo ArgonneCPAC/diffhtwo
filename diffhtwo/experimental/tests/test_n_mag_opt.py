085c7f8dc0fc95f6ce1bc8e9fc7a7b8f8aa5f7d6
Kumail Zaidi <kumail.tufts@gmail.com>
2026-03-20 16:58:59 -0400

    update args

--- diffhtwo/experimental/tests/test_n_mag_opt.py
+++ diffhtwo/experimental/tests/test_n_mag_opt.py
@@ -215,8 +215,6 @@ halpha_loss = n_mag_opt.get_halpha_loss(
     lg_halpha_LF_target,
     lg_halpha_Lbin_edges,
     lg_n_thresh,
-    zmin,
-    zmax,
     lc_halopop["z_obs"],
     lc_halopop["t_obs"],
     lc_halopop["mah_params"],
