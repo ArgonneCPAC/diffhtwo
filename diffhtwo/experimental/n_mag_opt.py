6e0f40ac356e45ff44673da7b855332fdf7c99b7
Kumail Zaidi <kumail.tufts@gmail.com>
2026-03-20 21:03:21 -0400

    Update n_mag_opt.py

--- diffhtwo/experimental/n_mag_opt.py
+++ diffhtwo/experimental/n_mag_opt.py
@@ -555,7 +555,7 @@ def get_halpha_loss(
 
     sig = jnp.diff(lg_halpha_Lbin_edges) / 2
     sig = sig.reshape(sig.size, 1)
-    print("lg_halpha_Lbin_edges"{}, lg_halpha_Lbin_edges)
+    print("lg_halpha_Lbin_edges:{}", lg_halpha_Lbin_edges)
     print("sig:{}", sig)
     _, halpha_N = emline_luminosity.get_emline_luminosity_func(
         L_halpha_cgs, lc_nhalos, sig=sig, lgL_bin_edges=lg_halpha_Lbin_edges
