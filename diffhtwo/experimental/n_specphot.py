6e7ce58994df8990b10c0dec010f3560339b9bbb
Kumail Zaidi <kumail.tufts@gmail.com>
2026-04-12 23:59:08 -0500

    Update n_specphot.py

--- diffhtwo/experimental/n_specphot.py
+++ diffhtwo/experimental/n_specphot.py
@@ -4,7 +4,6 @@ from diffsky.merging import merging_model
 from diffstar.defaults import FB
 from dsps.cosmology import DEFAULT_COSMOLOGY
 from jax import jit as jjit
-from jax.debug import print
 
 from . import diffndhist as diffndhist2
 from . import emline_luminosity
