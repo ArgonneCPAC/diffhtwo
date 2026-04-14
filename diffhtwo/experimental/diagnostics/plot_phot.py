e1b9118495667340ef014d473cf22e89588ff2b5
Kumail Zaidi <kumail.tufts@gmail.com>
2026-04-14 13:07:55 -0500

    Update plot_phot.py

--- diffhtwo/experimental/diagnostics/plot_phot.py
+++ diffhtwo/experimental/diagnostics/plot_phot.py
@@ -159,92 +159,94 @@ def plot_n_mag(
                 frac_cat,
             )
 
-    for i in range(0, n_bands):
-        sigma = np.std(dataset_mags_z1[:, i])
-        lower_limit = np.mean(dataset_mags_z1[:, i]) - (4 * sigma)
-        upper_limit = np.mean(dataset_mags_z1[:, i]) + (4 * sigma)
-        if i == n_bands - 1:
-            upper_limit = mag_thresh
-        mag_bin_edges = np.arange(
-            lower_limit,
-            upper_limit,
-            dmag,
-        )
-        ax[z, i].set_xlim(lower_limit, upper_limit)
-        ax_offset[z, i].set_xlim(lower_limit, upper_limit)
+        for i in range(0, n_bands):
+            sigma = np.std(dataset_mags_z1[:, i])
+            lower_limit = np.mean(dataset_mags_z1[:, i]) - (4 * sigma)
+            upper_limit = np.mean(dataset_mags_z1[:, i]) + (4 * sigma)
+            if i == n_bands - 1:
+                upper_limit = mag_thresh
+            mag_bin_edges = np.arange(
+                lower_limit,
+                upper_limit,
+                dmag,
+            )
+            ax[z, i].set_xlim(lower_limit, upper_limit)
+            ax_offset[z, i].set_xlim(lower_limit, upper_limit)
 
-        lc_phot1_hist = ax[z, i].hist(
-            obs_mag1,
-            weights=weights1 * (1 / lc_data.lc_vol_mpc3),
-            bins=mag_bin_edges,
-            histtype="step",
-            color=color1,
-            alpha=alpha1,
-            label=label1,
-            lw=lw + 1,
-        )
-
-        # model 2
-        if param_collection2 is not None:
-            ax[z, i].hist(
-                obs_mag2,
-                weights=weights2 * (1 / lc_data.lc_vol_mpc3),
+            lc_phot1_hist = ax[z, i].hist(
+                obs_mag1[:, i],
+                weights=weights1 * (1 / lc_data.lc_vol_mpc3),
                 bins=mag_bin_edges,
                 histtype="step",
-                color=color2,
-                alpha=alpha2,
-                lw=lw,
-                label=label2,
+                color=color1,
+                alpha=alpha1,
+                label=label1,
+                lw=lw + 1,
             )
 
-        # data
-        data_hist = ax[z, i].hist(
-            dataset_mags_z[:, i],
-            weights=np.ones_like(dataset_mags_z[:, i]) * (1 / data_vol_mpc3),
-            bins=mag_bin_edges,
-            color=color_data,
-            lw=lw,
-            alpha=alpha_data,
-            label="FENIKS-UDS",
-        )
+            # model 2
+            if param_collection2 is not None:
+                ax[z, i].hist(
+                    obs_mag2[:, i],
+                    weights=weights2 * (1 / lc_data.lc_vol_mpc3),
+                    bins=mag_bin_edges,
+                    histtype="step",
+                    color=color2,
+                    alpha=alpha2,
+                    lw=lw,
+                    label=label2,
+                )
 
-        """ax_offset"""
-        mag_bin_centers = (mag_bin_edges[1:] + mag_bin_edges[:-1]) / 2
-        offset = data_hist[0] / lc_phot1_hist[0]
-        ax_offset[z, i].plot(mag_bin_centers, offset, lw=2, color="k")
-        ax_offset[z, i].set_ylim(0.09, 10.1)
-        ax_offset[z, i].set_yticks([0.1, 0.2, 0.5, 1, 2, 5, 10])
-        ax_offset[z, i].set_yscale("log")
-        ax_offset[z, i].set_xlabel(dimension_labels[i], fontsize=fontsize)
-        ax_offset[z, i].tick_params(axis="both", direction="in", labelsize=labelsize)
+            # data
+            data_hist = ax[z, i].hist(
+                dataset_mags_z[:, i],
+                weights=np.ones_like(dataset_mags_z[:, i]) * (1 / data_vol_mpc3),
+                bins=mag_bin_edges,
+                color=color_data,
+                lw=lw,
+                alpha=alpha_data,
+                label="FENIKS-UDS",
+            )
 
-        ax[z, i].set_yscale("log")
-        ax[z, i].set_xlabel(dimension_labels[i], fontsize=fontsize)
-        ax[z, i].set_ylim(1e-6, 5e-3)
-        ax[z, i].tick_params(axis="both", direction="in", labelsize=labelsize)
+            """ax_offset"""
+            mag_bin_centers = (mag_bin_edges[1:] + mag_bin_edges[:-1]) / 2
+            offset = data_hist[0] / lc_phot1_hist[0]
+            ax_offset[z, i].plot(mag_bin_centers, offset, lw=2, color="k")
+            ax_offset[z, i].set_ylim(0.09, 10.1)
+            ax_offset[z, i].set_yticks([0.1, 0.2, 0.5, 1, 2, 5, 10])
+            ax_offset[z, i].set_yscale("log")
+            ax_offset[z, i].set_xlabel(dimension_labels[i], fontsize=fontsize)
+            ax_offset[z, i].tick_params(
+                axis="both", direction="in", labelsize=labelsize
+            )
 
-        ax_offset_yticks = np.array([0.2, 0.5, 1, 2, 5])
-        ax_offset[z, i].set_yticks(ax_offset_yticks)
-        ax_offset[z, i].axhspan(
-            ax_offset_yticks[1], ax_offset_yticks[3], color="orange", alpha=0.5
-        )
-        ax_offset[z, i].axhspan(
-            ax_offset_yticks[0], ax_offset_yticks[1], color="r", alpha=0.5
-        )
-        ax_offset[z, i].axhspan(
-            ax_offset_yticks[3], ax_offset_yticks[4], color="r", alpha=0.5
-        )
-        ax_offset[z, i].axhspan(0, ax_offset_yticks[0], color="r", alpha=0.8)
-        ax_offset[z, i].axhspan(ax_offset_yticks[4], 10, color="r", alpha=0.8)
+            ax[z, i].set_yscale("log")
+            ax[z, i].set_xlabel(dimension_labels[i], fontsize=fontsize)
+            ax[z, i].set_ylim(1e-6, 5e-3)
+            ax[z, i].tick_params(axis="both", direction="in", labelsize=labelsize)
 
-        if i != 0:
-            ax[z, i].set_yticklabels([])
-            ax_offset[z, i].set_yticklabels([])
-        if z != n_zbins - 1:
-            ax[z, i].set_xticklabels([])
-            ax_offset[z, i].set_xticklabels([])
-        if i == 0:
-            ax_offset[z, i].set_yticklabels(["5x", "2x", "1x", "2x", "5x"])
+            ax_offset_yticks = np.array([0.2, 0.5, 1, 2, 5])
+            ax_offset[z, i].set_yticks(ax_offset_yticks)
+            ax_offset[z, i].axhspan(
+                ax_offset_yticks[1], ax_offset_yticks[3], color="orange", alpha=0.5
+            )
+            ax_offset[z, i].axhspan(
+                ax_offset_yticks[0], ax_offset_yticks[1], color="r", alpha=0.5
+            )
+            ax_offset[z, i].axhspan(
+                ax_offset_yticks[3], ax_offset_yticks[4], color="r", alpha=0.5
+            )
+            ax_offset[z, i].axhspan(0, ax_offset_yticks[0], color="r", alpha=0.8)
+            ax_offset[z, i].axhspan(ax_offset_yticks[4], 10, color="r", alpha=0.8)
+
+            if i != 0:
+                ax[z, i].set_yticklabels([])
+                ax_offset[z, i].set_yticklabels([])
+            if z != n_zbins - 1:
+                ax[z, i].set_xticklabels([])
+                ax_offset[z, i].set_xticklabels([])
+            if i == 0:
+                ax_offset[z, i].set_yticklabels(["5x", "2x", "1x", "2x", "5x"])
 
     ax[0, -1].legend(
         framealpha=0.5,
@@ -464,13 +466,13 @@ def plot_n(
             if i == 0:
                 ax_offset[z, i].set_yticklabels(["5x", "2x", "1x", "2x", "5x"])
 
-        ax[0, -1].legend(
-            framealpha=0.5,
-            loc="upper left",
-            bbox_to_anchor=(-2, 1.4),
-            ncols=3,
-            fontsize=legend_fontsize,
-        )
+    ax[0, -1].legend(
+        framealpha=0.5,
+        loc="upper left",
+        bbox_to_anchor=(-2, 1.4),
+        ncols=3,
+        fontsize=legend_fontsize,
+    )
 
     fig.supylabel("\u03d5 [Mpc$^{-3}$]", fontsize=fontsize)
     fig.savefig(savedir + "/phot_fit_" + savedir.split("/")[-1] + ".pdf")
