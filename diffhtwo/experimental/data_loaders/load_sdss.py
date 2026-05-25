153b48a42d642b6ffa7b6b3594965225d59f9da5
Kumail Zaidi <kumail.tufts@gmail.com>
2026-05-25 17:54:57 -0400

    Update load_sdss.py

--- diffhtwo/experimental/data_loaders/load_sdss.py
+++ diffhtwo/experimental/data_loaders/load_sdss.py
@@ -91,7 +91,7 @@ def get_lh_centroids(dataset):
     lh_centroids = lh_centroids[redshift_mask & r_mask]
 
     redshift = [0.02, 0.065, 0.11, 0.155, 0.2]
-    r_mins = [12, 13.5, 14.5, 15.3, 16]
+    r_mins = [12, 12.5, 14, 14.5, 15]
     coeffs = np.polyfit(redshift, r_mins, deg=2)
     r_min = np.poly1d(coeffs)
     r_bright = lh_centroids[:, -2] > r_min(lh_centroids[:, -1])
