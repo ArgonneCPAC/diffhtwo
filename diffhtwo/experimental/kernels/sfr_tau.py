1cb91b81abb583c14ad3064e6659ac1fde3e1586
Kumail Zaidi <kumail.tufts@gmail.com>
2026-08-06 17:40:27 -0400

    Create sfr_tau.py

---
+++ diffhtwo/experimental/kernels/sfr_tau.py
@@ -0,0 +1,35 @@
+from jax import jit as jjit
+from jax import numpy as jnp
+from jax import vmap
+
+vmap_interp = jjit(vmap(jnp.interp, in_axes=(None, None, 0)))
+
+
+@jjit
+def frac_stellar_mass_tau(ssp_weights, lg_age_gyr, tau_gyr):
+    age_gyr = 10**lg_age_gyr
+
+    age_weights = jnp.sum(ssp_weights, axis=1)
+    cdf = jnp.cumsum(age_weights, axis=1)
+    cum_weight_tau = vmap_interp(tau_gyr, age_gyr, cdf)
+    return cum_weight_tau
+
+
+@jjit
+def compute_logsfr_tau(ssp_weights, lg_age_gyr, logsm_obs, tau_gyr=0.1):
+    """
+    Input:
+        ssp_weights:
+            SSP age-metallicity weights array of shape (n_gal, n_met, n_age)
+        lg_age_gyr:
+            SSP stellar age array of shape (n_age, )
+        tau_gyr: Timescale to compute SFR. Default:100Myr
+    Returns:
+        log(SFR) in a given timescale tau
+    """
+    frac_sm_obs = frac_stellar_mass_tau(ssp_weights, lg_age_gyr, tau_gyr)
+
+    sm_obs = 10**logsm_obs
+    tau_yr = tau_gyr * 1e9
+    sfr = (frac_sm_obs * sm_obs) / tau_yr
+    return jnp.log10(sfr)
