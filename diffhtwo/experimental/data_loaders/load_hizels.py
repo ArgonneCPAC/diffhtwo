be576220fddffa78c551ec7bae3c889ba9c81611
Kumail Zaidi <kumail.tufts@gmail.com>
2026-09-02 10:37:03 -0500

    lgmp_min=9.0 for h-alpha fitting

--- diffhtwo/experimental/data_loaders/load_hizels.py
+++ diffhtwo/experimental/data_loaders/load_hizels.py
@@ -32,7 +32,7 @@ def get_hizels_data(
     tcurves,
     halpha_wave_aa,
     num_halos=100,
-    lgmp_min=10.0,
+    lgmp_min=9.0,
     lgmp_max=15.0,
     lc_sky_area_degsq=100,
     n_z_phot_table=15,
