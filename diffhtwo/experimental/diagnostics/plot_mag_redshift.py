ccffa2d21301ae6b1fc19b90a7f387ab9d76cbc1
Kumail Zaidi <kumail.tufts@gmail.com>
2026-09-25 20:34:24 -0500

    linting

--- diffhtwo/experimental/diagnostics/plot_mag_redshift.py
+++ diffhtwo/experimental/diagnostics/plot_mag_redshift.py
@@ -436,7 +436,6 @@ def compare_models_in_mag_z_sfr(
     ran_key, lc_halo_key = jran.split(ran_key, 2)
     z_phot_table = np.linspace(z_min, z_max, 25)
     tcurves = dataset.filter_info.tcurves
-    mag_thresh = dataset.filter_info.mag_thresh
     mags_labels = dataset.mags_labels
 
     args = (
